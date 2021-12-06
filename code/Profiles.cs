46c516647d8edb7791dfd4c869b0eda3a5be087f
Crayolas <92621225+Crayolas@users.noreply.github.com>
2021-12-06 01:18:32 -0500

    Delete code directory

--- code/Profiles.cs
+++
@@ -1,9 +0,0 @@
-using Sandbox;
-namespace Roleplay{
-    public partial class Profile : BaseNetworkable{
-        [Net]
-        public int CharacterID{get;set;}
-        [Net]
-        public string Name {get;set;} 
-    }
-}
